591c0539c162733e31c46f7c2061a2b1f405a4ac
antara1865 <antaramane091@gmail.com>
2026-08-31 22:18:05 +0530

    Implement student admission eligibility checks
    Added student eligibility criteria for admission based on marks and age.

--- Lab2.py
+++ Lab2.py
@@ -1,41 +1,32 @@
-print("***Groceery Shop Billing Calculator***")
+print("\n\n-------------Student  Eligibility Critaria for Admission-------------\")
 
-rice_qty=float(input("Enter the quantity of rice(in kg):"))
-rice_price_per_kg=60
-rice_total= rice_qty*rice_price_per_kg
+name=input("Enter Student Name:")
+marks=float(input("Enter Student Marks/Percentage:"))
+age=int(input("Enter Student Age:"))
 
-sugar_qty=float(input("Enter the quantity of  sugar(in kg):"))
-sugar_price_per_kg=100
-sugar_total= sugar_qty*sugar_price_per_kg
+print("Admission Result")
+print("--------------------")
 
-salt_qty=float(input("Enter the quantity of salt(in kg):"))
-salt_price_per_kg=70
-salt_total= salt_qty*salt_price_per_kg
-
-oil_qty=float(input("Enter the quantity of oil(in litre):"))
-oil_price_per_lit=200
-oil_total= oil_qty*oil_price_per_lit
-
-print("***Display Bill Details***")
-print("rice:",rice_total)
-print("sugar:",sugar_total)
-print("salt:",salt_total)
-print("oil:",oil_total)
-
-Total_Bill = rice_total+sugar_total+salt_total+oil_total
-print("Total Bill:", Total_Bill)
-
-Discount=0
-if Total_Bill>=2000:
-    Discount=Total_Bill*0.1
-    print("Discount:",Discount)
-
-elif Total_Bill>=1000:
-    Discount=Total_Bill*0.05
-    print("Discount:",Discount)
-
+if marks>=50:
+    if age>= 17:
+        if marks>=90:
+            print(name,"Admission Approved")
+            print("Cousre Alloted:Computer Science And Enggineering")
+            print(name,"Congratulation you have get Scholarship:100%")
+        elif marks>=75:
+            print(name,"Admission Approved")
+            print("Cousre Alloted:Information Technology")
+            print(name,"Congratulation you have get Scholarship:50%")
+        elif marks>=60:
+            print(name,"Admission Approved")
+            print("Cousre Alloted:Artifical Intellgence")
+            print(name,"Congratulation you have get Scholarship:25%")
+        else:
+            print(name,"Admission Approved")
+            print("Cousre Alloted:Electrical Engineering")
+            print(name,"No Scholarship")       
 else:
-    print("No Discount")
-
-Final_Bill= Total_Bill-Discount
-print("Final Bill:",Final_Bill)
+    print(name,"Admission Rejected")
+    print(" No Cousre Alloted")
+    print("Reason:Minimum Marks should be above 50%")
+    print("Reason:Minimum Age should be above 17")     
